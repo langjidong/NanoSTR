f301463d1fc5e7701dac7149c67d6c32f3f8657a
langjidong <langjidong@hotmail.com>
2022-12-14 10:04:26 +0800

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,2 @@
+cat ONT.fastq.tar.bz2.aa ONT.fastq.tar.bz2.ab ONT.fastq.tar.bz2.ac ONT.fastq.tar.bz2.ad ONT.fastq.tar.bz2.ae ONT.fastq.tar.bz2.af|tar jxvf -
 perl NanoStrTyping.pl -fq ONT.fastq -step_size 10 -configure str.bed -tmp_dir tmpdir -process 4
